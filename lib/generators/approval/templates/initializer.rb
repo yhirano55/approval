01af16c9b6d4e40419bef84c4582486acbf19f14
Yoshiyuki Hirano <yhirano@me.com>
2017-08-01 11:57:54 +0900

    Modified initializer template

--- lib/generators/approval/templates/initializer.rb
+++ lib/generators/approval/templates/initializer.rb
@@ -1,4 +1,10 @@
 Approval.configure do |config|
+  # Your user model name (e.g. User, AdminUser, Member, default: User)
   config.user_class_name = "User"
+
+  # Maximum characters of comment for reason (default: 2000)
   config.comment_maximum = 2000
+
+  # Permit to respond to own request? (default: false)
+  config.permit_to_respond_to_own_request = false
 end
