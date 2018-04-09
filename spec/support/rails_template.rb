2aa3103e7f20e754476d12af85b117ae076387d0
Yoshiyuki Hirano <yhirano@me.com>
2018-04-09 18:22:00 +0900

    Fix rails_template

--- spec/support/rails_template.rb
+++ spec/support/rails_template.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 generate :"approval:install"
 generate :model, "User name:string"
+inject_into_file "app/models/user.rb", "  acts_as_approval_user\n", before: "end"
 generate :model, "Book name:string"
 inject_into_file "app/models/book.rb", "  acts_as_approval_resource\n", before: "end"
 run "rm -rf spec"
