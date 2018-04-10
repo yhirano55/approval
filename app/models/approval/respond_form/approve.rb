6196a15a8a95673403189f9340cc294d288207af
Yoshiyuki Hirano <yhirano@me.com>
2018-04-10 17:10:21 +0900

    Don't execute in RespondForm::Approve

--- app/models/approval/respond_form/approve.rb
+++ app/models/approval/respond_form/approve.rb
@@ -10,7 +10,6 @@ module Approval
             request.lock!
             request.assign_attributes(state: :approved, approved_at: Time.current, respond_user_id: user.id)
             request.comments.new(user_id: user.id, content: reason)
-            request.items.each(&:apply)
             yield(request)
           end
         end
